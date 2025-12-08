c1cb4668f08545cd1c47f72f652e3e868f069b35
Vamsi yerra <yerravamsi77@gmail.com>
2025-12-08 20:40:53 +0530

    Add files via upload

---
+++ StrongNumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class StrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        int originalNum = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is a Strong Number.");
+        } else {
+            System.out.println(originalNum + " is not a Strong Number.");
+        }
+        sc.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+    
+}
